8a926a45337e8290548a95b016e1bececfd23ca9
Rajnish rai <rajnishrai277123@gmail.com>
2026-08-02 09:56:39 +0530

    first

---
+++ PrintHello.java
@@ -0,0 +1,13 @@
+public class PrintHello{
+    static void print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println("Rajnish");
+        print(n-1);
+
+    }
+    public static void main(String[] args){
+      print(10);
+    }
+}
